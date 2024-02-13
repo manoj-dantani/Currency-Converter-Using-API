e5704bb33687bdf0ff7e330d37274514d2f385d1
Manoj Dantani <manojdantani2002@gmail.com>
2024-02-13 21:32:24 +0530

    flag updated

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 
 const BASEURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-let val1=document.getElementById("val1");
-let val2=document.getElementById("val2");
-let btn=document.getElementById("btn_crt");
-let result = document.getElementById("res");
+let amtVal = document.getElementById("amtVal");
+let dropdownVal = document.querySelectorAll("select");
+let result = document.querySelector(".result-container p");
+let btngetRate = document.querySelector("button");
 
+for(let option of dropdownVal){
+    for(let currencyCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currencyCode;
+        newOption.value = currencyCode;
+        if(option.name === "from" && currencyCode === "CAD"){
+            newOption.selected = "selected";
+        }else if(option.name === "to" && currencyCode === "INR"){
+            newOption.selected = "selected";
+        }
+        option.append(newOption);
+    }
+    option.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let currencyCode = element.value;
+    let countryCode = countryList[currencyCode];
+    let imgSrc = element.parentElement.querySelector("img");
+    imgSrc.src = `https://flagsapi.com/${countryCode}/shiny/64.png`;g
+};
 
 const getData = async () => {
 
@@ -17,4 +40,4 @@ const getData = async () => {
 
 };
 
-btn.addEventListener("click",getData);
+btngetRate.addEventListener("click",getData);
