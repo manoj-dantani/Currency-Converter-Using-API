65c56880fbced428e406af7a533b8dba993bbb46
Manoj Dantani <manojdantani2002@gmail.com>
2024-02-13 23:47:42 +0530

    Completed

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 const BASEURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-let amtVal = document.getElementById("amtVal");
+let amtVal = document.querySelector("input");
 let dropdownVal = document.querySelectorAll("select");
 let result = document.querySelector(".result-container p");
+let fromCurrency = document.querySelector(".country1 select");
+let toCurrency = document.querySelector(".country2 select");
 let btngetRate = document.querySelector("button");
+result.style.fontSize = "17px";
+result.style.fontWeight = "700"; 
+
+amtVal.value = 1;
 
 for(let option of dropdownVal){
     for(let currencyCode in countryList){
@@ -27,17 +33,19 @@ const updateFlag = (element) => {
     let currencyCode = element.value;
     let countryCode = countryList[currencyCode];
     let imgSrc = element.parentElement.querySelector("img");
-    imgSrc.src = `https://flagsapi.com/${countryCode}/shiny/64.png`;g
+    imgSrc.src = `https://flagsapi.com/${countryCode}/shiny/64.png`;
 };
 
 const getData = async () => {
+    if(amtVal.value === "" || amtVal.value < 1){
+        amtVal.value = 1;
+    }
 
-    let URL = `${BASEURL}/${val1.value}/${val2.value}.json`;
+    let URL = `${BASEURL}/${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`;
     let response = await fetch(URL);
-    console.log(response.status); 
     let data = await response.json();
-    console.log(data);
-
+    let rate = data[toCurrency.value.toLowerCase()];
+    result.innerText = `${amtVal.value} ${fromCurrency.value} = ${rate.toFixed(2)*amtVal.value} ${toCurrency.value} as ${data.date}`;
 };
 
 btngetRate.addEventListener("click",getData);
